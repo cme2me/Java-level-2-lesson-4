2cadd011b9c610771ff3c9c2b69f4f93376ac4a3
cme2me <max.zhakov@gmail.com>
2022-02-14 17:05:06 +0300

    Homework is done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    public TextArea chatField;
+    @FXML
+    public TextField messageField;
+    @FXML
+    public Button sendBtn;
+
+    @FXML
+    public void sendMsg(ActionEvent actionEvent) {
+        chatField.appendText(messageField.getText()+"\n");
+        messageField.clear();
+        sendBtn.requestFocus();
+    }
 }
